8372817b8d6f8bfa3939442850fc97f5c15d715c
Tong Lin <tonglin.t91@gmail.com>
2017-04-10 14:56:07 -0400

    Q506 Relative Ranks solved

---
+++ DSAPlayground-LeetCode.playground/Pages/500-549.xcplaygroundpage/Contents.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+//506 Relative Ranks
+
+func findRelativeRanks(_ nums: [Int]) -> [String] {
+    var dict: [Int:Int] = [:]
+    var finalArr: [String] = nums.map{String($0)}
+    for index in 0..<nums.count{
+        dict[nums[index]] = index
+    }
+    var count = 1
+    for num in nums.sorted(by: >){
+        switch count {
+        case 1:
+            finalArr[dict[num]!] = "Gold Medal"
+        case 2:
+            finalArr[dict[num]!] = "Silver Medal"
+        case 3:
+            finalArr[dict[num]!] = "Bronze Medal"
+        default:
+            finalArr[dict[num]!] = String(count)
+        }
+        count += 1
+    }
+    
+    return finalArr
+}
+
+
+findRelativeRanks([3,4,2,1,5])
